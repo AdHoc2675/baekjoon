f9c95b76210962924356a438c9ead5e065678a52
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 22:49:11 +0900

    [Bronze II] Title: 음계, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main { //제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int arr[] = new int[8];
+        String str = " ";
+
+        for (int i = 0; i < 8; i++) {
+            arr[i] = s1.nextInt();
+        }
+
+        for (int i = 0; i < 7; i++) {
+            if (arr[i] == arr[i + 1] - 1) {
+                str = "ascending";
+            } else if (arr[i] == arr[i + 1] + 1) {
+                str = "descending";
+            } else {
+                str = "mixed";
+                break;
+            }
+        }
+
+        System.out.println(str);
+    }
+}
