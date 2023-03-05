ef6b0aaf1e6835648f2f70e7927eac729d85b35a
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 23:27:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2496 ms, Memory: 256588 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main { // 제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int n, m;
+        int range1, range2;
+
+        n = s1.nextInt();
+        m = s1.nextInt();
+
+        int arr[] = new int[n + 1];
+
+        arr[0] = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + s1.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            range1 = s1.nextInt();
+            range2 = s1.nextInt();
+
+            System.out.println(arr[range2] - arr[range1 - 1]);
+        }
+    }
+}
