ad392eb2a101fdbba42af0688809b7443fb2e850
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 22:43:53 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 292 ms, Memory: 18056 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main { //제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int n = s1.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
