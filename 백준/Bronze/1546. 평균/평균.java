c272f8fff972d26dae6fa3a753b07fccaf05456a
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 22:42:49 +0900

    [Bronze I] Title: 평균, Time: 348 ms, Memory: 23184 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main { // 제출할 때는 클래스 이름을 Main으로 바꿀것
+    public static void main(String[] args) {
+        int n;
+        int m = 0;
+        double average = 0;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        n = keyboard.nextInt();
+
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = keyboard.nextDouble();
+            if (m < arr[i]) {
+                m = (int) arr[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] / m * 100;
+            average = average + arr[i];
+        }
+
+        average = average / n;
+
+        System.out.println(average);
+
+        keyboard.close();
+    }
+}
