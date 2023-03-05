0712940d3c275dd4ab41d970dd120ab6f5e11caf
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 23:36:38 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main { // 제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        int n = s1.nextInt();
+
+        System.out.println(n - 1946);
+    }
+}
