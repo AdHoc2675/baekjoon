9fc7fd7464f6bf4f21263b90a919b0c7c2962332
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 23:19:48 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	
+	while (1) {
+		cin >> n >> m;
+		if (n == 0 && m == 0) {
+			break;
+		}
+		cout << n + m << "\n";
+	}
+	
+}
