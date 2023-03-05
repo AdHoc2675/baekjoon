cb18b1ec7ba23903e3116004b27881b118e7caf2
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 23:36:12 +0900

    [Bronze V] Title: A+B - 9, Time: 228 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main { // 제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        long n, m;
+
+        n = s1.nextLong();
+        m = s1.nextLong();
+
+        System.out.println(n + m);
+    }
+}
