c20087dfb51be7c3d80b769ba1e023599254c503
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 22:32:20 +0900

    [Bronze V] Title: A+B - 2, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
