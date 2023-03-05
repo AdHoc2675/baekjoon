f0585d1eb32ead8f7b190064a7bcf59711661879
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 23:23:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main { // 제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        long a, b, c;
+
+        a = s1.nextLong();
+        b = s1.nextLong();
+        c = s1.nextLong();
+
+        System.out.println(a + b + c);
+
+    }
+}
