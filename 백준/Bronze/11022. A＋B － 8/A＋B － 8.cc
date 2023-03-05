91efb6543037ae1271c90906f198f39229d90235
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 23:20:15 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, T;
+
+	cin >> T;
+
+	for (int i = 1; i <= T; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+	}
+}
