55c343ce9f043dbffd7fd72773b39c3318993d69
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-03-05 22:49:25 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main { // 제출시 Main
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        String str;
+
+        str = s1.nextLine();
+
+        int count = str.length();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1) {
+                if (i < str.length() - 2) {
+                    if (str.charAt(i) == 'd' && str.charAt(i + 1) == 'z' && str.charAt(i + 2) == '=') {
+                        count = count - 2;
+                        i = i + 2;
+                    }
+                }
+                if (str.charAt(i) == 'c' && str.charAt(i + 1) == '=') {
+                    count--;
+                    i++;
+                } else if (str.charAt(i) == 'c' && str.charAt(i + 1) == '-') {
+                    count--;
+                    i++;
+                } else if (str.charAt(i) == 'd' && str.charAt(i + 1) == '-') {
+                    count--;
+                    i++;
+                } else if (str.charAt(i) == 'l' && str.charAt(i + 1) == 'j') {
+                    count--;
+                    i++;
+                } else if (str.charAt(i) == 'n' && str.charAt(i + 1) == 'j') {
+                    count--;
+                    i++;
+                } else if (str.charAt(i) == 's' && str.charAt(i + 1) == '=') {
+                    count--;
+                    i++;
+                } else if (str.charAt(i) == 'z' && str.charAt(i + 1) == '=') {
+                    count--;
+                    i++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
