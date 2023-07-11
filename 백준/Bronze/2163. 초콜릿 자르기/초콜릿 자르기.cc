03a9ee3ad3a21c601f9475476d2effa1a96ad75d
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-07-11 16:10:04 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int m;
+
+    cin >> n;
+    cin >> m;
+
+    int answer = n * m - 1;
+
+    cout << answer;
+
+    return 0;
+}
