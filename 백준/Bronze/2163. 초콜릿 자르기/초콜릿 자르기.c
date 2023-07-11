49f8b48d451318f04980e24761c6e2ed9e2a3b55
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-07-11 16:11:23 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int m;
+
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    int answer = n * m - 1;
+
+    printf("%d", answer);
+
+    return 0;
+}
