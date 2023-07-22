2e1d30ddfac5199445d3cda0dcb66a22d5cfd490
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-07-22 15:50:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int movieTitle = 666;
+    int indexOfMovie = 1;
+
+    while (true) {
+        // indexOfMovie가 n과 같으면 movieTitle이 n번째 종말의 숫자가 포함된것
+        if (indexOfMovie == n) {
+            break;
+        }
+        movieTitle++;
+        int temp = movieTitle;
+
+        while (true) {
+            if (temp == 0) {
+                break;
+            }
+
+            // 마지막 세 자리가 666인지 확인
+            if (temp % 1000 == 666) {
+                // 종말의 숫자면 indexOfMovie 1 증가
+                indexOfMovie++;
+                break;
+            }
+            else {
+                // 종말의 숫자가 아니면, 일의 자리수를 없앰
+                temp = temp / 10;
+            }
+        }
+    }
+
+    cout << movieTitle;
+
+    return 0;
+}
