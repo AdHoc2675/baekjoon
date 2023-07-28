27204028fb3a4335a750012dc52d2f6982a3be0e
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-07-28 13:49:48 +0900

    [Bronze II] Title: 카이사르 암호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5598. 카이사르 암호/카이사르 암호.c
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.c
@@ -2,26 +2,26 @@
 #include <string.h>
 
 int main() {
-    char name[1000];
+    char str[1000];
 
-	scanf("%s", name);
+	scanf("%s", str);
 
-	for (int i = 0; i < strlen(name); i++) {
-        if (name[i] == 'A') {
-            name[i] = 'X';
+	for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == 'A') {
+            str[i] = 'X';
         }
-        else if (name[i] == 'B') {
-            name[i] = 'Y';
+        else if (str[i] == 'B') {
+            str[i] = 'Y';
         }
-        else if (name[i] == 'C') {
-            name[i] = 'Z';
+        else if (str[i] == 'C') {
+            str[i] = 'Z';
         }
         else {
-            name[i] = name[i] - 3;
+            str[i] = str[i] - 3;
         }
 	}
 
-    printf("%s", name);
+    printf("%s", str);
 
 	return 0;
 }
