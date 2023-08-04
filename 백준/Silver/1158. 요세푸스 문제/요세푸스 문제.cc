5c228e6c619551972e84137e509b619d5e38d96f
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-08-04 12:48:14 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	queue<int>q;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	cout << "<";
+
+	while (true) {
+		if (q.size() <= 1) {
+			break;
+		}
+
+		for (int i = 1; i < k; i++) {
+			int tmp = q.front();
+
+			q.pop(); //앞에서 빼서
+			q.push(tmp); //뒤에 다시 넣는다
+		}
+
+		cout << q.front() << ", ";
+		q.pop();
+	}
+
+	cout << q.front() << ">\n";
+}
