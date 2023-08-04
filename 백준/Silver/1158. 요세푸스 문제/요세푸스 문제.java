b86be9b716c53f4d2cdf81c66c2386f3ea4ec3e9
AdHoc2675 <54134687+AdHoc2675@users.noreply.github.com>
2023-08-04 13:51:10 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 844 ms, Memory: 299164 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, k;
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+
+        System.out.print("<");
+
+        while (true) {
+            if (q.size() <= 1) {
+                break;
+            }
+
+            for (int i = 1; i < k; i++) {
+                int tmp = q.poll();
+
+                q.add(tmp); // 뒤에 다시 넣는다
+            }
+
+            System.out.print(q.poll() + ", ");
+        }
+
+        System.out.print(q.poll() + ">\n");
+    }
+}
