1d21061b62ef915723a157e4e58e69cc243525af
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2025-09-19 13:24:41 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -8,16 +8,17 @@ int main()
 {
     string str;
     int n;
-    int m = 1234567891;
+    long long m = 1234567891;
     long long r = 1;
 
     cin >> n;
     cin >> str;
 
     long long result = 0;
+
     for (int i = 0; i < n; i++)
     {
-        result = result + ((str[i] - 96) * r) % m;
+        result = (result + ((str[i] - 'a' + 1) * r) % m) % m;
 
         r = (r * 31) % m;
     }
